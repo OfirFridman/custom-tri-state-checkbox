4862d176ca1c0bdc53926e244b9dd983fd11bd09
ofirrifo <ofirrifo@gmail.com>
2016-01-08 21:48:38 +0200

    1. added gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,94 @@
 /**
  * Created by ofir on 1/8/2016.
  */
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
+
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+const path = {
+    app:{
+        sass:"app/styles/custom-tri-state-checkbox.scss",
+        css:"app/css",
+        js:"app/**/*.js"
+    },
+    dist:{
+        styles:"dist/styles",
+        scripts:"dist/scripts"
+    }
+};
+
+// Styles
+gulp.task('styles', function() {
+    return sass(path.app.sass, { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest(path.app.css))
+        .pipe(gulp.dest(path.dist.styles))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest(path.dist.styles))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+    return gulp.src(path.app.js)
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(path.dist.scripts))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.dist.scripts))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Images
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Clean
+gulp.task('clean', function() {
+    return del([path.dist.styles, path.dist.scripts, 'dist/images']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch(path.app.sass, ['styles']);
+
+    // Watch .js files
+    gulp.watch(path.app.js, ['scripts']);
+
+    // Watch image files
+    gulp.watch('src/images/**/*', ['images']);
+
+    // Create LiveReload server
+    livereload.listen();
+
+    // Watch any files in dist/, reload on change
+    gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
